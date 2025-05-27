c0c57f88173df70e124e3ad6a953a6510c88fe68
Igor Sousa <112203229+SalowQ@users.noreply.github.com>
2025-05-27 16:48:56 -0300

    feat: criar middleware para content-type e consumo de streams

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  await json(req, res);
+
   console.log("oi");
+
   return res.writeHead(404).end();
 });
 
